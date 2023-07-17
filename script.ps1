08d414f34f1b03961d319a237e1d72ba849f684b
Louis Ruocco <louisruocco1@gmail.com>
2023-07-17 21:49:43 +0100

    added wildcard to newName variable and able to successfully remove directory from desktop after copy

--- script.ps1
+++ script.ps1
@@ -4,11 +4,10 @@ $allFiles = Get-ChildItem $desktop
 foreach ($file in $allFiles){
     if($file -like "code*"){
         $path = "$desktop\$file"
-        $newName = $file.Name -replace "code - ", ''
+        $newName = $file.Name -replace "code*", ''
         Rename-Item $path -NewName $newName
         $newPath = "$desktop\$newName"
         robocopy $newPath "E:\Code\$newName" /tee /np /w:0 /r:0 /xj /nfl /ndl
-    } else {
-        
+        Remove-Item $newPath -Recurse
     }
 }
