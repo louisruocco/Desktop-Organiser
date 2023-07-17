96d0e6bcd4b14c1f6fd443b36a5377892967823a
Louis Ruocco <louisruocco1@gmail.com>
2023-07-17 21:45:45 +0100

    able to change folder/file names and move them to another directory

---
+++ script.ps1
@@ -0,0 +1,14 @@
+$desktop = "$home\Desktop"
+$allFiles = Get-ChildItem $desktop
+
+foreach ($file in $allFiles){
+    if($file -like "code*"){
+        $path = "$desktop\$file"
+        $newName = $file.Name -replace "code - ", ''
+        Rename-Item $path -NewName $newName
+        $newPath = "$desktop\$newName"
+        robocopy $newPath "E:\Code\$newName" /tee /np /w:0 /r:0 /xj /nfl /ndl
+    } else {
+        
+    }
+}
